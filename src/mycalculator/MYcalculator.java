22e2fedf45c9895b10d3b468dbec00716532bed0
SergioPerez <dark07perez@gmail.com>
2019-04-29 17:19:49 -0500

    primer commit

---
+++ src/mycalculator/MYcalculator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycalculator;
+
+/**
+ *
+ * @author sergio
+ */
+public class MYcalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        
+        
+        power(1, 2);
+    }
+    
+    
+    static int power(int n, int p) throws Exception{
+        int res =  0;
+        if(n<0 || p<0 ) throw new Exception("n and p should be non-negative");
+        else res = (int) Math.pow(n, p);
+
+        return res;
+
+        
+    }
+
+    
+}
